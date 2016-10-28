1b45128fb7ce7b1dfc215dcc0b1c1198182889f7
fiddlydigital <philipagannon@gmail.com>
2016-10-28 21:27:43 +0000

    Minor change as previously was breaking the build

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,7 @@ const hub = new HubRegistry([conf.path.tasks('*.js')]);
 // Tell gulp to use the tasks just loaded
 gulp.registry(hub);
 
-
-gulp.task('inject', gulp.series('clean', gulp.parallel('styles', 'scripts'), 'inject'));
+gulp.task('inject', gulp.series(gulp.parallel('styles', 'scripts'), 'inject'));
 gulp.task('build', gulp.series('partials', gulp.parallel('inject', 'other'), 'build'));
 gulp.task('test', gulp.series('scripts', 'karma:single-run'));
 gulp.task('test:auto', gulp.series('watch', 'karma:auto-run'));
@@ -31,12 +30,11 @@ function watch(done) {
     'bower.json'
   ], gulp.parallel('inject'));
 
-  gulp.watch(conf.path.src(conf.ngModule + '/**/*.html'), gulp.series('partials', reloadBrowserSync));
+  gulp.watch(conf.path.src('app/**/*.html'), gulp.series('partials', reloadBrowserSync));
   gulp.watch([
     conf.path.src('**/*.less'),
     conf.path.src('**/*.css')
   ], gulp.series('styles'));
-  
   gulp.watch(conf.path.src('**/*.js'), gulp.series('inject'));
   done();
 }
